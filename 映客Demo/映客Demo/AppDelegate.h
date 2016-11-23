ec5e2876c2ad0b6ebfe177502d27ece6781911c3
Style_yw <664739052@qq.com>
2016-11-23 12:22:37 +0800

    初始化映客Demo项目

---
+++ 映客Demo/映客Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  映客Demo
+//
+//  Created by wyw on 16/11/23.
+//  Copyright © 2016年 wyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 映客Demo/映客Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  映客Demo
+//
+//  Created by wyw on 16/11/23.
+//  Copyright © 2016年 wyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
